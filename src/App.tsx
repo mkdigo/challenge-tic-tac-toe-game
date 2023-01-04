317bab9995b9f139c11949e77ec32beaef158b84
rodrigo mukudai <mkdigo@gmail.com>
2023-01-04 17:42:25 +0900

    version 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,176 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+
+import './App.scss';
+
+type Player = 'x' | 'o';
+type Position = Player | null;
+type Orientation = 'horizontal' | 'vertical' | 'diagonal';
+type Winner = {
+  player: Player;
+  orientation: Orientation;
+  index: number;
+};
+
+const initialPositions = [null, null, null, null, null, null, null, null, null];
+const winConditions = {
+  horizontal: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ],
+  vertical: [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ],
+  diagonal: [
+    [0, 4, 8],
+    [2, 4, 6],
+  ],
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [positions, setPositions] = useState<Position[]>(initialPositions);
+
+  const [initialPlayer, setInitialPlayer] = useState<Player>('x');
+  const [currentPlayer, setCurrentPlayer] = useState<Player>('x');
+  const [winner, setWinner] = useState<Winner>();
+  const [score, setScore] = useState({
+    x: 0,
+    o: 0,
+  });
+
+  function handleClick({
+    position,
+    index,
+  }: {
+    position: Position;
+    index: number;
+  }) {
+    if (position || winner) return;
+
+    const newPositions = [...positions];
+    newPositions[index] = currentPlayer;
+    setPositions(newPositions);
+    setCurrentPlayer((prev) => (prev === 'x' ? 'o' : 'x'));
+  }
+
+  function handleRestart() {
+    setPositions(initialPositions);
+    setWinner(undefined);
+
+    const nextPlayer: Player = initialPlayer === 'x' ? 'o' : 'x';
+    setCurrentPlayer(nextPlayer);
+    setInitialPlayer(nextPlayer);
+  }
+
+  function checkIfHasWinner(player: Player): boolean {
+    let hasWinner = false;
+
+    const orientations: Array<Orientation> = [
+      'horizontal',
+      'vertical',
+      'diagonal',
+    ];
+
+    function checkFromOrientation({
+      condition,
+      orientation,
+      index,
+    }: {
+      condition: number[];
+      orientation: Orientation;
+      index: number;
+    }) {
+      if (
+        positions[condition[0]] === player &&
+        positions[condition[1]] === player &&
+        positions[condition[2]] === player
+      ) {
+        setWinner({
+          player,
+          orientation,
+          index,
+        });
+
+        hasWinner = true;
+      }
+    }
+
+    orientations.forEach((orientation) => {
+      winConditions[orientation].forEach((condition, index) => {
+        checkFromOrientation({ condition, index, orientation });
+      });
+    });
+
+    return hasWinner;
+  }
+
+  useEffect(() => {
+    if (checkIfHasWinner('x')) {
+      setScore((prev) => ({
+        ...prev,
+        x: prev.x + 1,
+      }));
+    }
+    if (checkIfHasWinner('o')) {
+      setScore((prev) => ({
+        ...prev,
+        o: prev.o + 1,
+      }));
+    }
+  }, [positions]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main>
+      <div className='container'>
+        <h1>Tic-tac-toe</h1>
+
+        <section className='score'>
+          <ul>
+            <strong>Score</strong>
+            <li>
+              <strong>x :</strong>
+              <span>{score.x}</span>
+            </li>
+            <li>
+              <strong>o :</strong>
+              <span>{score.o}</span>
+            </li>
+          </ul>
+          <div>
+            {winner && <span className='winner'>Winner</span>}
+            <span className='turn'>
+              Player {winner ? winner.player : currentPlayer}
+            </span>
+          </div>
+        </section>
+
+        <ul className='gameBox'>
+          {winner && (
+            <div
+              className={`winLine ${winner.orientation}`}
+              style={{
+                ['--position' as any]: winner.index,
+              }}
+            ></div>
+          )}
+          {positions.map((position, index) => (
+            <li
+              key={index}
+              className={!position && !winner ? 'isClickable' : ''}
+              onClick={() => handleClick({ position, index })}
+            >
+              {position}
+            </li>
+          ))}
+        </ul>
+        <button type='button' onClick={handleRestart}>
+          Restart
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
